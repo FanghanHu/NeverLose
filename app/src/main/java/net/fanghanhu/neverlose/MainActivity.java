dc1d712b390064a23d84b3c5ee4247e931b1b512
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-26 12:13:40 -0600

    added privacy policy

--- app/src/main/java/net/fanghanhu/neverlose/MainActivity.java
+++ app/src/main/java/net/fanghanhu/neverlose/MainActivity.java
@@ -2,13 +2,20 @@ package net.fanghanhu.neverlose;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -53,13 +60,64 @@ public class MainActivity extends AppCompatActivity
         }
 
         if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String notice1 = "This app uses your location data in the background when you connect or disconnect to a Bluetooth device, the data will be retained on your device and is not collected by us in any way. ";
+
+            String notice2 = "<span>By downloading or using the app, you agree to the NeverLose <a href='https://github.com/FanghanHu/NeverLose/blob/master/PrivacyPolicy.md'>Terms and Conditions</a> " +
+                    "and <a href='https://github.com/FanghanHu/NeverLose/blob/master/PrivacyPolicy.md'>Privacy Policy</a>.</span>";
+
+
+            final LinearLayout layout = new LinearLayout(MainActivity.this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            layoutParams.setMargins(40, 40, 40, 40);
+
+            final TextView message = new TextView(MainActivity.this);
+            message.setText(notice1);
+
+            final TextView message2 = new TextView(MainActivity.this);
+            message2.setText(Html.fromHtml(notice2));
+            message2.setMovementMethod(LinkMovementMethod.getInstance());
+
+            layout.addView(message, layoutParams);
+            layout.addView(message2, layoutParams);
+
             //No background location permission
-            //ask for it
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            }, 0);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Accept Terms")
+                    .setView(layout)
+                    //yes button
+                    .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                            {
+                                //ask for background location usage
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                                        Manifest.permission.ACCESS_FINE_LOCATION,
+                                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                                }, 1);
+                            } else {
+                                //Ask for permissions to use locations
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                }, 0);
+                            }
+                        }
+                    })
+
+                    //no button
+                    .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Exit program
+                            System.exit(0);
+                        }
+                    })
+                    .show();
         }
     }
 
